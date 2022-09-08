e5ea404b44e5f0ff1f5665d7bde11e6029629c50
Ikennaf1 <iiikbros202@gmail.com>
2022-09-08 07:55:22 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Prints to the stderr
+ * Return: Returns 1 on success
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
