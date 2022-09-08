5a1044eff2ccb9632331f242be3334a98c6a55f9
Ikennaf1 <iiikbros202@gmail.com>
2022-09-08 05:59:35 -0700

    Mandatory tasks on Low Level Programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This program prints specified text using puts function
+ * Return: Returns 0 on successful completion
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This prints specified text to stdout using printf function
+ * Return: Returns 0 on success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
