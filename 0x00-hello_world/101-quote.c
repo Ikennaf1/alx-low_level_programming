89dfb0b62d1c3094230319d5f51a4ebdb07a8542
Ikennaf1 <iiikbros202@gmail.com>
2022-09-08 11:54:27 -0700

    Fix: Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,8 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" \
-			- Dora Korpar, 2015-10-19\n");
+	char str[] = "- Dora Korpar, 2015-10-19\n";
+
+	fprintf(stderr, "and that piece of art is useful\" %s", str);
 	return (1);
 }
