d54a0f7eeffd64634518c124a0a0d10974738ec4
Ikennaf1 <iiikbros202@gmail.com>
2022-09-22 14:10:48 +0100

    100-task

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * rot13 - encrypts a string
+ * @s: Input string
+ * Return: final string
+ */
+char *rot13(char *s)
+{
+	char *p;
+	int count;
+
+	char alpha[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+		'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+		'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+		'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+		'u', 'v', 'w', 'x', 'y', 'z'};
+
+	char swap[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+		'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+		'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+		'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+		'l', 'm'};
+
+	p = s;
+
+	while (*p != '\0')
+	{
+		count = 0;
+		while (count < 52)
+		{
+			if (*p == alpha[count])
+			{
+				*p = swap[count];
+				break;
+			}
+			count++;
+		}
+		p++;
+	}
+	return (s);
+}
