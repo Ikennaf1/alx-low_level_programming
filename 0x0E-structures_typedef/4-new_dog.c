3bd1ce7b2604c1d913f4d7cd80749a7387a2ac3f
Ikennaf1 <iiikbros202@gmail.com>
2022-10-10 15:38:40 +0100

    Dog.h fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "dog.h"
 
 /**
  * new_dog - Creates a new dog
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,14 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct dog - Dog struct
  * @name: Name of dog
  * @age: Age of dog
- * @owner: Owner of dog
+ * @owner: Owner name
  *
  * Description: A dog struct
  */
@@ -17,8 +20,14 @@ struct dog
 };
 
 /**
- * dog_t - Typedef for dog struct
+ * dog_t - typedef for dog struct
  */
 typedef struct dog dog_t;
 
-#endif
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* MAIN_H */
