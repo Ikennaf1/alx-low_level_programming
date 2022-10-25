9bb72c2dc3531da8ab6ac4902f059f687ccbd0d0
Ikennaf1 <iiikbros202@gmail.com>
2022-10-25 16:19:13 +0100

    9 fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -37,8 +37,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			free(new);
 			return (NULL);
 		}
-		if (idx > count)
-			return (NULL);
 		if (count == (idx - 1))
 		{
 			new->next = temp->next;
