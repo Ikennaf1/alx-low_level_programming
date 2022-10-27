7717f1d0c2aef182df0555c490de12ea35a55a27
Ikennaf1 <iiikbros202@gmail.com>
2022-10-27 22:39:13 +0100

    0 fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -26,20 +26,20 @@ unsigned int _strlen(const char *s)
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int count = 1, number = 0; 
-	int _len = (_strlen(b) - 1);
+	unsigned int count = 0, number = 0, _len = (_strlen(b) - 1);
 
 	if (b == NULL)
 		return (0);
 
-	while (_len >= 0)
+	while (_len)
 	{
 		if (b[_len] != 48 && b[_len] != 49)
 			return (0);
 
-		number += (b[_len] - '0') * count;
+		if (b[_len] == 49)
+			number += 1 << count;
 
-		count *= 2;
+		count++;
 		_len--;
 	}
 	return (number);
