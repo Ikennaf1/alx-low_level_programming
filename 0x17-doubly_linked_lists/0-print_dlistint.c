ad8188e5e0a19bba99149013433f5fae2ed2c98a
Ikennaf1 <iiikbros202@gmail.com>
2022-12-08 19:48:54 +0100

    Betty checks

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,7 +10,7 @@ size_t print_dlistint(const dlistint_t *h)
 	size_t num = 0;
 	const dlistint_t *t = h;
 
-	while(t != NULL)
+	while (t != NULL)
 	{
 		printf("%d\n", t->n);
 		num++;
--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,13 +1,14 @@
 #include "lists.h"
 
 /**
- * free_dlistint_t - frees a list
+ * free_dlistint - frees a list
  * @head: list head
  * Return: Nothing
  */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *temp;
+
 	if (head == NULL)
 		return;
 	temp = head;
