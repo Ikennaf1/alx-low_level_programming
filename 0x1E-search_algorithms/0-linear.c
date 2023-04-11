7383f6a8c63f0762b220c6f99ae3d860adf49191
Ikennaf1 <iiikbros202@gmail.com>
2023-04-11 08:57:04 +0100

    Linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Searches an array for a needle using linear search
+ * @array: The given array
+ * @size: The size of the array
+ * @value: The value to search for
+ */
+int linear_search(int *array, size_t size, int value)
+{
+    int index, sz = size;
+
+    if (array != NULL)
+    {    
+        for (index = 0; index < sz; index++)
+        {
+            printf("Value checked array[%u] = [%d]\n", index, array[index]);
+
+            if (array[index] == value)
+                return (index);
+        }
+    }
+    return (-1);   
+}
