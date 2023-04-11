edf10cb2e600b97bfe1ea7ff3f350bd831266904
Ikennaf1 <iiikbros202@gmail.com>
2023-04-11 09:22:56 +0100

    Fixed docstring

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -5,6 +5,7 @@
  * @array: The given array
  * @size: The size of the array
  * @value: The value to search for
+ * Return: The index of value if found, or -1
  */
 int linear_search(int *array, size_t size, int value)
 {
